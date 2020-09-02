7abec7df73b9d96e13a47a8a88b6b10abba1e96e
Zach Drummond <zachdrummond3@gmail.com>
2020-09-02 09:40:46 -0400

    Copied Develop Files

---
+++ script.js
@@ -0,0 +1,18 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+function generatePassword(){
+  //All your code goes here.
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
